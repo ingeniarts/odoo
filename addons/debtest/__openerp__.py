ff1ec095947a896bb71ee80b63ed4e4c6e3a31c4
SAP <louisp.letourneau@hotmail.com>
2016-03-23 14:13:11 -0400

    debranding odoo 23 mars

--- addons/debtest/__openerp__.py
+++ addons/debtest/__openerp__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "test",
+    'name': "debtest",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -25,6 +25,9 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/debt.xml',
+    ],
+     'qweb': [
+        'views/web.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
