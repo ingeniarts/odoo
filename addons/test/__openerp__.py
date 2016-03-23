34b87b1da4d185038418aad81d69ca8f7e73be4d
SAP <louisp.letourneau@hotmail.com>
2016-03-23 11:07:24 -0400

    testing odoo debrand module

---
+++ addons/test/__openerp__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "test",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "",
+    'website': "",
+
+    # Categories
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'web'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        <!--'views/views.xml',-->
+        'views/debt.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
