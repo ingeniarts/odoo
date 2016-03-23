1127fdcf4a241f46fa94bb6bb4b2d7fc532e511f
SAP <louisp.letourneau@hotmail.com>
2016-03-23 11:45:57 -0400

    yo

--- addons/debtest/__openerp__.py
+++ addons/debtest/__openerp__.py
@@ -23,7 +23,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        <!--'views/views.xml',-->
+        'views/views.xml',
         'views/debt.xml',
     ],
     # only loaded in demonstration mode
