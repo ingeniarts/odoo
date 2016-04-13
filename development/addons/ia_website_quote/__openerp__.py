4b3963e58aa56556037c03b503030d54c6b2d0d2
ingeniarts <louisp.letourneau@hotmail.com>
2016-04-13 15:25:40 -0400

    ia_website, ia_website_quote, and some FIX!

--- development/addons/ia_website_quote/__openerp__.py
+++ development/addons/ia_website_quote/__openerp__.py
@@ -4,7 +4,7 @@
     'installable': True,
     'auto_install': False,
 
-    'name': "ia_website",
+    'name': "ia_website_quote",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -22,14 +22,18 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'web', 'purchase', 'sale', 'website_form','website_partner', 'crm', 'website_quote'],
+    'depends': ['base', 'web', 'purchase', 'sale', 'website_form','website_partner', 'crm', 'website_quote',
+                'website', 'mail', 'web_tip', 'payment', 'website_portal_sale', 'website_mail'],
+
+
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/ia_website_footer.xml',
-        'views/ia_website_autofooter.xml',
-        'views/ia_website_homepage.xml',
+        #'security/ir.model.access.csv',
+        #'views/ia_website_quote_pricing.xml',
+        'views/ia_website_quote_content.xml',
+        #'data/ia_website_quotation_data.xml',
+        'views/ia_website_quote_quotation.xml',
     ],
      #'qweb': [
      #   'static/src/xml/ia_website.xml',
